131a7a12ab382eb3ba1bb7c526840c73ecbd880d
ElvDev05 <vasquezelverth@gmail.com>
2024-06-05 04:31:49 -0500

    feat(product): Implement queries for Brand and Category in Application

--- src/main/java/pe/edu/upc/comperu_platform/products/application/internal/queryservices/BrandQueryServiceImpl.java
+++ src/main/java/pe/edu/upc/comperu_platform/products/application/internal/queryservices/BrandQueryServiceImpl.java
@@ -1,4 +1,22 @@
 package pe.edu.upc.comperu_platform.products.application.internal.queryservices;
 
-public class BrandQueryServiceImpl {
+import pe.edu.upc.comperu_platform.products.domain.model.entities.Brand;
+import pe.edu.upc.comperu_platform.products.domain.model.queries.GetBrandByIdQuery;
+import pe.edu.upc.comperu_platform.products.domain.services.BrandQueryService;
+import pe.edu.upc.comperu_platform.products.infrastructure.persistence.jpa.repositories.BrandRepository;
+
+import java.util.Optional;
+
+public class BrandQueryServiceImpl implements BrandQueryService {
+
+    private final BrandRepository brandRepository;
+
+    public BrandQueryServiceImpl(BrandRepository brandRepository) {
+        this.brandRepository = brandRepository;
+    }
+
+    @Override
+    public Optional<Brand> handle(GetBrandByIdQuery query) {
+        return brandRepository.findById(query.brandId());
+    }
 }
--- src/main/java/pe/edu/upc/comperu_platform/products/application/internal/queryservices/CategoryQueryServiceImpl.java
+++ src/main/java/pe/edu/upc/comperu_platform/products/application/internal/queryservices/CategoryQueryServiceImpl.java
@@ -1,4 +1,23 @@
 package pe.edu.upc.comperu_platform.products.application.internal.queryservices;
 
-public class CategoryQueryServiceImpl {
+import pe.edu.upc.comperu_platform.products.domain.model.entities.Category;
+import pe.edu.upc.comperu_platform.products.domain.model.queries.GetCategoryByIdQuery;
+import pe.edu.upc.comperu_platform.products.domain.services.CategoryQueryService;
+import pe.edu.upc.comperu_platform.products.infrastructure.persistence.jpa.repositories.CategoryRepository;
+
+import java.util.Optional;
+
+public class CategoryQueryServiceImpl implements CategoryQueryService {
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryQueryServiceImpl(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+
+    @Override
+    public Optional<Category> handle(GetCategoryByIdQuery query) {
+        return categoryRepository.findById(query.categoryId());
+    }
 }
