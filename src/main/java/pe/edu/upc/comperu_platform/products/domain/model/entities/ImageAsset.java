d5aee93263143d8cf9f2291ec39ec3ab5b5d7612
ElvDev05 <vasquezelverth@gmail.com>
2024-06-26 20:12:40 -0500

    fix(Review): fix logic in bounded context review

--- src/main/java/pe/edu/upc/comperu_platform/products/domain/model/entities/ImageAsset.java
+++ src/main/java/pe/edu/upc/comperu_platform/products/domain/model/entities/ImageAsset.java
@@ -6,11 +6,12 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import pe.edu.upc.comperu_platform.products.domain.model.aggregates.Product;
+import pe.edu.upc.comperu_platform.shared.domain.model.entities.AuditableModel;
 
 @Getter
 @Setter
 @Entity
-public class ImageAsset {
+public class ImageAsset extends AuditableModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
